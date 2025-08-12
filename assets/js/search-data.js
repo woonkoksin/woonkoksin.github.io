526c3a1f2c0923f2352a7aec68712726cf83a3ed
woonkoksin <ywang489@connect.hkust-gz.edu.cn>
2025-08-12 06:38:26 +0000

    Deploying to gh-pages from @ woonkoksin/woonkoksin.github.io@3e09285b7ba01e750a9a78e8da6a280bf9f84abd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,16 +405,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-elected-as-a-member-of-the-global-young-academy",
-          title: 'Elected as a Member of the Global Young Academy.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-elected-as-a-member-of-the-global-young-academy",
+          title: 'Elected as a Member of the Global Young Academy.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
