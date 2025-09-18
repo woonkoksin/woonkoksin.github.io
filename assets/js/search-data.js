9a72fa944b8b7ed90fc3c060879a89e7c2c85e77
woonkoksin <ywang489@connect.hkust-gz.edu.cn>
2025-09-18 07:32:26 +0000

    Deploying to gh-pages from @ woonkoksin/woonkoksin.github.io@fba74e56583b764e7aaaf97b7ccee9a6aeef3b91 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-projects",
           title: "projects",
           description: "",
           section: "Navigation",
